d7f06c4470edff86607a4144ac9e5319091a8eee
João <joaocruz375@gmail.com>
2024-07-09 16:25:15 -0300

    estruturando home

---
+++ src/components/CarrosselSlider.vue
@@ -0,0 +1,57 @@
+<template>
+  <swiper
+    :modules="modules"
+    :slides-per-view="1"
+    :space-between="25"
+    navigation
+    loop
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <SwiperSlide>
+      <img src="../../public/assets/carro-3.jpg" alt="">
+    </SwiperSlide>
+    <SwiperSlide>
+      <img src="../../public/assets/carro-2.jpg" alt="">
+    </SwiperSlide>
+    <SwiperSlide>
+      <img src="../../public/assets/carro-4.jpg" alt="">
+    </SwiperSlide>
+  </swiper>
+</template>
+<script>
+  import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  import 'swiper/css/bundle';
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      const onSwiper = (swiper) => {
+        console.log(swiper);
+      };
+      const onSlideChange = () => {
+        console.log('slide change');
+      };
+      return {
+        onSwiper,
+        onSlideChange,
+        modules: [Navigation, Pagination, Scrollbar, A11y],
+      };
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  img {
+    max-width: 500px;
+    max-height: 500px;
+    display: block;
+    margin: 0 auto;
+  }
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,20 @@
 <template>
-  <HeaderComponent />
+  <main>
+    <HeaderComponent />
+    <section class="d-flex flex-column justify-content-center align-items-center mt-5">
+      <h2>Serviços em até 4 horas!</h2>
+      <article class="d-flex w-50">
+        <CarrosselSlider class="my-5" />
+      </article>
+    </section>
+  </main>
 </template>
 
 <script>
 export default {
   components: {
     HeaderComponent: () => import("../components/HeaderComponent.vue"),
+    CarrosselSlider: () => import("../components/CarrosselSlider.vue"),
   }
 }
 </script>
