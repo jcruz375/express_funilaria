15a08e4cea92fd9d9c7c23a3366eaba2161142f2
João <joaocruz375@gmail.com>
2024-07-09 16:43:44 -0300

    novo link no header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,7 +2,7 @@
   <nav>
       <ul>
         <li>
-          <a class="logo" href="#" target="blank" rel="noopener noreferrer">
+          <a class="logo" href="#" rel="noopener noreferrer">
             <img
               src="../../public/assets/logo.svg"
               alt="Logo funilaria"
@@ -12,14 +12,17 @@
       </ul>
       <ul>
         <li>
-          <a href="#" target="blank" rel="noopener noreferrer">Serviços</a>
+          <a href="#" rel="noopener noreferrer">Serviços</a>
         </li>
         <li>
-          <a href="#" target="blank" rel="noopener noreferrer">Resultados</a>
+          <a href="#" rel="noopener noreferrer">Resultados</a>
+        </li>
+        <li class="d-flex align-items-baseline">
+          <b-icon icon="pin-map" class="me-2"></b-icon>
+          <a href="#" rel="noopener noreferrer">Localização</a>
         </li>
         <li>
-          <a class="btn" href="#" target="blank" rel="noopener noreferrer">
-            <!-- <FaWhatsapp /> -->
+          <a class="btn" href="#" rel="noopener noreferrer">
             <i class="fab fa-whatsapp"></i>
             Entre em contato
           </a>
