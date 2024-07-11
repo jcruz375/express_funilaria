04fc30b56c4b7fdfd9405d51e71e3dbb55862489
João <joaocruz375@gmail.com>
2024-07-10 22:24:26 -0300

    alterando ordem

--- src/components/CarrosselSlider.vue
+++ src/components/CarrosselSlider.vue
@@ -9,55 +9,64 @@
     @slideChange="onSlideChange"
   >
     <SwiperSlide>
-      <img src="../../public/assets/carro-3.jpg" alt="Imagem de um carro sendo reparado na oficina Funilaria Express">
+      <img
+        src="../../public/assets/carro-4.jpg"
+        alt="Imagem de um carro sendo reparado na oficina Funilaria Express"
+      />
     </SwiperSlide>
     <SwiperSlide>
-      <img src="../../public/assets/carro-2.jpg" alt="Imagem de um carro sendo reparado na oficina Funilaria Express">
+      <img
+        src="../../public/assets/carro-3.jpg"
+        alt="Imagem de um carro sendo reparado na oficina Funilaria Express"
+      />
     </SwiperSlide>
     <SwiperSlide>
-      <img src="../../public/assets/carro-4.jpg" alt="Imagem de um carro sendo reparado na oficina Funilaria Express">
+      <img
+        src="../../public/assets/carro-2.jpg"
+        alt="Imagem de um carro sendo reparado na oficina Funilaria Express"
+      />
     </SwiperSlide>
   </swiper>
 </template>
 <script>
-  import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 
-  import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Swiper, SwiperSlide } from "swiper/vue";
 
-  import 'swiper/css/bundle';
+import "swiper/css/bundle";
 
-  export default {
-    components: {
-      Swiper,
-      SwiperSlide,
-    },
-    setup() {
-      const onSwiper = (swiper) => {
-        console.log(swiper);
-      };
-      const onSlideChange = () => {
-        console.log('slide change');
-      };
-      return {
-        onSwiper,
-        onSlideChange,
-        modules: [Navigation, Pagination, Scrollbar, A11y],
-      };
-    },
-  };
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log("slide change");
+    };
+    return {
+      onSwiper,
+      onSlideChange,
+      modules: [Navigation, Pagination, Scrollbar, A11y],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  img {
-    max-width: 500px;
-    max-height: 500px;
-    display: block;
-    margin: 0 auto;
-  }
+img {
+  max-width: 500px;
+  max-height: 500px;
+  display: block;
+  margin: 0 auto;
+}
 
-  @media (max-width: 768px) {
-    img {
-      max-width: 320px;
-    }
+@media (max-width: 768px) {
+  img {
+    max-width: 320px;
   }
+}
 </style>
